1df236f148b56aa6255e1f2ea684d96a75dd761c
kaizen23 <neuralbox@op.pl>
2017-01-01 10:07:43 +0100

    create class Comparer to List<Banks>

--- BankParse/RootBiloComparer.cs
+++ BankParse/RootBiloComparer.cs
@@ -1,12 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace BankParse
 {
     class RootBiloComparer
     {
+        public List<Bank> ComparerRootBilo(RootBank root, RootBank bilo)
+        {
+            var firstNotSecond = root.Banks.Except(bilo.Banks).ToList();
+
+            return firstNotSecond;
+        }
+
+        public List<Bank> ComparerBiloRoot(RootBank bilo, RootBank root)
+        {
+            
+            var secondNotFirst = bilo.Banks.Except(root.Banks).ToList();
+
+            return secondNotFirst;
+        }
     }
 }
